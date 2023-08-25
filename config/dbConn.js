8da878bac084b02b3357909a9760d7507b605fc2
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-08-25 15:00:06 -0700

    Edited connection string

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://mongotut:<password>@cluster0.avtw6dr.mongodb.net/techNotesDB?retryWrites=true&w=majority")
+        await mongoose.connect("mongodb://0.0.0.0:27017/")
     } catch (err) {
         console.log(err)
     }
